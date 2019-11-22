6d26ba0301330370638a5ec2c11f8caae8b75dbd
Jonathan Tieken <jonathan@oneclickretail.com>
2019-11-22 14:38:37 -0700

    nginx proxy server demo

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -1,5 +1,7 @@
-from flask import Flask, jsonify
+import os
+from flask import Flask, jsonify, send_from_directory, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug import secure_filename
 
 
 app = Flask(__name__)
@@ -21,3 +23,30 @@ class User(db.Model):
 @app.route("/")
 def hello_world():
     return jsonify(hello="world")
+
+
+@app.route("/static/<path:filename>")
+def staticfiles(filename):
+    return send_from_directory(app.config["STATIC_FOLDER"], filename)
+
+
+@app.route("/media/<path:filename>")
+def mediafiles(filename):
+    return send_from_directory(app.config["MEDIA_FOLDER"], filename)
+
+
+@app.route("/upload", methods=["GET", "POST"])
+def upload_file():
+    if request.method == "POST":
+        file = request.files["file"]
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config["MEDIA_FOLDER"], filename))
+    return f"""
+    <!DOCTYPE html>
+    <title>upload new File</title>
+    <form action="" method="post" enctype="multipart/form-data">
+        <p>
+            <input type="file" name="file"><input type="submit" value="Upload">
+        </p>
+    </form>
+    """
